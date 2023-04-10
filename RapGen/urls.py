c01160b961562088899705f841a2549a1f1d54fb
lukeanglin <lukeanglin@gmail.com>
2023-04-10 17:33:24 -0400

    got printing to terminal the correct rap, struggling to get it to display, think might b because url is like ?prompt={prompt} but not sure maybe need to tell django that somehow in urls.py

--- RapGen/urls.py
+++ RapGen/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, run_generate_rap
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('run-generate-rap/', run_generate_rap, name='run-generate-rap')
 ]
--- RapGen/views.py
+++ RapGen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -6,4 +7,15 @@ def home(request):
     # Do some processing here
     # context = {'message': 'Hello, world!'}
     # Render the HTML template with the given context data
-    return render(request, 'home.html') # context=context)
+    return render(request, 'home') # context=context)
+
+def generateRap(prompt):
+    # your python code here
+    return f"Rap generated here, using prompt {prompt}"
+
+def run_generate_rap(request):
+    prompt = request.GET.get('prompt')
+    print('prompt is ', prompt)
+    output = generateRap(prompt)
+    print('output is ', output)
+    return JsonResponse({'output': output})
