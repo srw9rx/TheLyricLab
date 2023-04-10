387933f43ce23b222c60f44a4ecc03dd01be3e15
lukeanglin <lukeanglin@gmail.com>
2023-04-10 13:38:30 -0400

    boilerplate at url /RapGen/home

--- RapGen/views.py
+++ RapGen/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # Do some processing here
+    # context = {'message': 'Hello, world!'}
+    # Render the HTML template with the given context data
+    return render(request, 'home.html') # context=context)
